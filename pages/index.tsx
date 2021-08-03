720f2bf7d5e9c1102827cad2fa23b940c51ae41f
Michael Brenndoerfer <mbnet@hotmail.de>
2021-08-03 21:07:58 +0800

    og cover image

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,21 +34,18 @@ export default function Home({ allItems }) {
             <Head>
                 <title>Data Science & Machine Learning | Terminology.me</title>
                 <meta name="description" content="Machine Learning and Data Science Terminology Explained" />
+                <meta property="og:locale" content="en_US" />
                 <meta property="og:site_name" content="Machine Learning and Data Science Terminology" />
                 <meta property="og:title” content=Machine Learning and Data Science Terminology" />
                 <meta property="og:description" content="Compact and easy to understand explaination of all data science abd machine learning related terms" />
                 <meta property="og:url" content="https://datascience.terminology.me" />
                 <meta property="og:type" content="website" />
-                <meta property="article:publisher" content="https://datascience.terminology.me" />
-                {/* <meta property="article:section" content="Coding" />
-                <meta property="article:tag" content="Data Science" /> */}
-                {/* <meta property="og:image" content="https://res.cloudinary.com/fay/image/upload/w_1280,h_640,c_fill,q_auto,f_auto/w_860,c_fit,co_rgb:232129,g_west,x_80,y_-60,l_text:Source%20Sans%20Pro_70_line_spacing_-10_semibold:Anyone%20Can%20Map!%20Inspiration%20and%20an%20introduction%20to%20the%20world%20of%20mapping/blog-social-card-1.1" /> */}
-                {/* <meta property="og:image:secure_url" content="https://res.cloudinary.com/fay/image/upload/w_1280,h_640,c_fill,q_auto,f_auto/w_860,c_fit,co_rgb:232129,g_west,x_80,y_-60,l_text:Source%20Sans%20Pro_70_line_spacing_-10_semibold:Anyone%20Can%20Map!%20Inspiration%20and%20an%20introduction%20to%20the%20world%20of%20mapping/blog-social-card-1.1" /> */}
-                {/* <meta property="og:image:width" content="1280" /> */}
-                {/* <meta property="og:image:height" content="640" /> */}
-                {/* <meta property="twitter:card" content="summary_large_image" /> */}
-                {/* <meta property="twitter:image" content="https://res.cloudinary.com/fay/image/upload/w_1280,h_640,c_fill,q_auto,f_auto/w_860,c_fit,co_rgb:232129,g_west,x_80,y_-60,l_text:Source%20Sans%20Pro_70_line_spacing_-10_semibold:Anyone%20Can%20Map!%20Inspiration%20and%20an%20introduction%20to%20the%20world%20of%20mapping/blog-social-card-1.1" /> */}
-                <meta property="twitter:site" content="@terminology.me" />
+                <meta property="og:image" content="https://datascience.terminology.me/assets/og/og_cover.jpg" />
+                <meta property="og:image:secure_url" content="https://datascience.terminology.me/assets/og/og_cover.jpg" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:image" content="https://datascience.terminology.me/assets/og/og_cover.jpg" />
 
                 <link rel="icon" href="/favicon.ico" />
 
